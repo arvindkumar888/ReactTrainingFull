5aa79d31c251053c5bdff4e53d629cb35093d2c8
arvindkumar888@gmail.com <arvindkumar888@gmail.com>
2019-05-02 14:08:13 +0530

    comm

--- reduxapp/src/index.js
+++ reduxapp/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -9,4 +9,23 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister();*/
+import {createStore} from 'redux';
+
+const reducer = function(state,action){
+    if(action.type === 'INC'){
+        return state+action.payload;
+    }
+    if(action.type === 'DEC'){
+        return state-action.payload
+    }
+    return state;
+}
+const store = createStore(reducer, 1);
+store.subscribe(()=>{
+    console.log("Store Changed: "+store.getState())
+});
+
+store.dispatch({type:"INC", payload:1});
+store.dispatch({type:"INC", payload:3});
+store.dispatch({type:"DEC", payload:21});
